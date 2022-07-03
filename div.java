b34d568a3bfb64312b28de26d181cd45bf3bf5f6
Sumanth Naik <sumanthmuttu524@gmail.com>
2022-07-03 21:12:48 +0530

    added div

---
+++ div.java
@@ -0,0 +1,12 @@
+public class div {
+    /**
+     * @param args
+     */
+    public static void main(String[]args)
+    {
+        final int a=800 ;
+        int b=90;
+        System.out.println(a/b);
+    }
+    
+}
