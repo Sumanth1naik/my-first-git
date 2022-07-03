82d269a526087db81d1fa309f44a7f70e52c0b70
Sumanth Naik <sumanthmuttu524@gmail.com>
2022-07-03 21:41:03 +0530

    it is about some command tips

---
+++ simple.java
@@ -0,0 +1,13 @@
+public class simple {
+    //if you try to open file in git hub simple comands
+    /* cd <file name> it will open excisting file,
+     * if you want open branch --> git branch <name of branch>
+     *                          -->then git branch   <it will create that>
+     * git add<file name>   -->adds the new programs to ready stage
+     * git commit -m "comment" -->it wiil upload to exsisting stage
+     * git checkout -->you can go  any branch
+     * git merge <branch name> -->will give to merge branch to main
+     * 
+     */
+    
+}
